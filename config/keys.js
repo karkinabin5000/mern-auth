b5a2c5508a51ff99480269bc85558ec112376b99
karkinabin5000 <karkinabin5000@gmail.com>
2019-01-29 17:22:17 -0600

    updated mongoURI to use localhost

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://nabin:teamlion128@ds143683.mlab.com:43683/department-evaluation-db",
+  ///mongoURI: "mongodb://nabin:teamlion128@ds143683.mlab.com:43683/department-evaluation-db",
+  mongoURI: "mongodb:localhost:27017/db",
   secretOrKey: "secret"
 };
